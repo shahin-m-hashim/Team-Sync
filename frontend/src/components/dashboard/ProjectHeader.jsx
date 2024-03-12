c261839ee3976db0b9d1dfd3a3a2ee83316f1384
Shahin M Hashim <shahinmhashim@gmail.com>
2024-03-13 00:34:43 +0530

    simulated project body layout with non empty project list
    (cherry picked from commit f144685274429d922ac7dc621b332dc19962f648)

--- frontend/src/components/dashboard/ProjectHeader.jsx
+++ frontend/src/components/dashboard/ProjectHeader.jsx
@@ -1,16 +1,19 @@
 export default function ProjectHeader() {
   return (
-    <div
-      id="projectHeader"
-      className="flex bg-[#1C1C1C] justify-between w-full py-3 px-5 text-sm"
-    >
-      <span>Name</span>
-      <span>Created</span>
-      <span>Icon</span>
-      <span>Progress</span>
-      <span>Status</span>
-      <span>Role</span>
-      <span>Settings</span>
+    <div>
+      <div
+        id="projectHeader"
+        className="grid grid-cols-[.5fr,.5fr,.5fr,1.5fr,.7fr,.5fr,.5fr,.5fr] bg-[#1C1C1C] w-full py-3 px-7 text-sm"
+      >
+        <span>Name</span>
+        <span>Created</span>
+        <span>Icon</span>
+        <span>Progress</span>
+        <span className="pl-3">Status</span>
+        <span className="pl-1">Role</span>
+        <span>Settings</span>
+        <span>Delete</span>
+      </div>
     </div>
   );
 }
