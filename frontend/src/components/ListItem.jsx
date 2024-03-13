922ffc6b7654b768c19121fea9deab5858bda512
Ajmal Sajeev <ajmal.sajeev7@gmail.com>
2024-03-13 15:49:28 +0530

    updated project list ui

--- frontend/src/components/ListItem.jsx
+++ frontend/src/components/ListItem.jsx
@@ -13,14 +13,15 @@ export default function ListItem({
   role,
 }) {
   return (
-    <div className="grid my-[1px] border-gray-700 border-b-[1px] grid-cols-[.5fr,.5fr,.5fr,1.5fr,.7fr,.5fr,.5fr,.5fr] w-full py-3 px-7 text-sm">
+    <div className="grid items-center my-[1px] border-gray-700 border-b-[1px] grid-cols-[1fr,.5fr,.5fr,1.5fr,.7fr,.7fr,.5fr,.5fr] w-full py-3 px-7 text-sm">
       <span>{name}</span>
-      <span>{createdDate}</span>
-      <div className="flex items-center pl-1">
+      <span className="pl-5">{createdDate}</span>
+      <div className="pl-14">
         <img src={icon} alt="project icon" />
       </div>
-      <div className="flex items-center">
-        <div className="inline-flex text-xs items-center justify-center bg-[#5030E5] size-7 rounded-[50%]">
+      <div className="flex items-center pl-1
+      ">
+        <div className="inline-flex text-xs items-center justify-center bg-[#5030E5] size-10 rounded-[50%]">
           {progress}%
         </div>
         <div className="h-2 w-[250px] rounded-l-none rounded-xl bg-white">
@@ -30,24 +31,26 @@ export default function ListItem({
           ></div>
         </div>
       </div>
-      <div
-        className={cn(
-          {
-            "bg-[#069843]": status === "Done",
-            "bg-[#e8be34]": status === "Not Started",
-            "bg-[#A87BFF]": status === "Pending",
-            "bg-[#B10F0F]": status === "Stopped",
-          },
-          "text-xs inline-flex items-center justify-center text-black h-[25px] w-[80px] font-semibold rounded-xl"
-        )}
-      >
-        <span>{status}</span>
+      <div className="pl-16">
+        <div
+          className={cn(
+            {
+              "bg-[#069843]": status === "Done",
+              "bg-[#e8be34]": status === "Not Started",
+              "bg-[#A87BFF]": status === "Pending",
+              "bg-[#B10F0F]": status === "Stopped",
+            },
+            "text-xs inline-flex items-center justify-center text-black h-[25px] w-[80px] font-semibold rounded-xl"
+          )}
+        >
+          <span>{status}</span>
+        </div>
       </div>
-      <span>{role}</span>
-      <button className="pl-3">
+      <span className="pl-14">{role}</span>
+      <button className="pl-14">
         <img src={settings} width={20} alt="settings icon" />
       </button>
-      <button className="pl-3">
+      <button className="pl-14">
         <img src={deleteIcon} width={20} alt="delete icon" />
       </button>
     </div>
--- frontend/src/components/dashboard/ProjectHeader.jsx
+++ frontend/src/components/dashboard/ProjectHeader.jsx
@@ -3,16 +3,16 @@ export default function ProjectHeader() {
     <div>
       <div
         id="projectHeader"
-        className="grid grid-cols-[.5fr,.5fr,.5fr,1.5fr,.7fr,.5fr,.5fr,.5fr] bg-[#1C1C1C] w-full py-3 px-7 text-sm"
+        className="grid grid-cols-[1fr,.5fr,.5fr,1.5fr,.7fr,.7fr,.5fr,.5fr] bg-[#1C1C1C] w-full py-3 px-7 text-sm"
       >
         <span>Name</span>
-        <span>Created</span>
-        <span>Icon</span>
-        <span>Progress</span>
-        <span className="pl-3">Status</span>
-        <span className="pl-1">Role</span>
-        <span>Settings</span>
-        <span>Delete</span>
+        <span className="pl-3">Created</span>
+        <span className="pl-8">Icon</span>
+        <span className="pl-5">Progress</span>
+        <span className="pl-20">Status</span>
+        <span className="pl-16">Role</span>
+        <span className="pl-10">Settings</span>
+        <span className="pl-10">Delete</span>
       </div>
     </div>
   );
