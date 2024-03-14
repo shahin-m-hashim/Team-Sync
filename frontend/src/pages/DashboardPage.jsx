4494117070fba6663a2bf96aa5128d0c3b62bd3f
Shahin M Hashim <shahinmhashim@gmail.com>
2024-03-14 13:48:17 +0530

    Simplified the functionality of projects by creating a project context, Modified the list Components and DashboardPage to use the project context, Created an empty list body component
    (cherry picked from commit 3f4aa13e1e084653e82ad64288b73c5f3422ef4e)

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,17 +1,17 @@
+import { useState } from "react";
 import SideBar from "@/components/dashboard/SideBar";
 import Navbar from "@/components/dashboard/Navbar";
 import ActivityCard from "@/components/cards/ActivityCard";
 import MessageCard from "@/components/cards/MessageCard";
 import StatusCard from "@/components/cards/StatusCard";
 import AddComponent from "@/components/AddComponent";
-import { useState } from "react";
 import ListHeader from "@/components/list/ListHeader";
 import ListBody from "@/components/list/ListBody";
 import ListSubHeader from "@/components/list/ListSubHeader";
+import ProjectProvider from "@/contexts/projectContext";
 
 export default function DashboardPage() {
   const [showAddPopUp, setShowAddPopUp] = useState(false);
-  const [listOnlyAdminProjects, setListOnlyAdminProjects] = useState(false);
   const [searchByName, setSearchByName] = useState(null);
 
   return (
@@ -27,21 +27,19 @@ export default function DashboardPage() {
           <ActivityCard />
           <MessageCard />
         </div>
-        <div
-          id="dashBody"
-          className="bg-[#141414] m-1 mt-0 rounded-lg text-white"
-        >
-          <ListHeader
-            setShowAddPopUp={setShowAddPopUp}
-            setListOnlyAdminProjects={setListOnlyAdminProjects}
-            setSearchByName={setSearchByName}
-          />
-          <ListSubHeader />
-          <ListBody
-            listOnlyAdminProjects={listOnlyAdminProjects}
-            searchByName={searchByName}
-          />
-        </div>
+        <ProjectProvider>
+          <div
+            id="dashBody"
+            className="bg-[#141414] m-1 mt-0 rounded-lg text-white"
+          >
+            <ListHeader
+              setShowAddPopUp={setShowAddPopUp}
+              setSearchByName={setSearchByName}
+            />
+            <ListSubHeader />
+            <ListBody searchByName={searchByName} />
+          </div>
+        </ProjectProvider>
       </div>
       {showAddPopUp && (
         <AddComponent name="Project" setShowAddPopUp={setShowAddPopUp} />
