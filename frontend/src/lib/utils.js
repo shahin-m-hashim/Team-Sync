c102e17b044fc5d97dba9bdb961598aceedc38ae
Shahin M Hashim <shahinmhashim@gmail.com>
2024-03-23 22:00:34 +0530

    implemented a secure way to store and use local storage easily
    (cherry picked from commit 2b9a62dc8ee094b0952bae86d67a0bfd3306b125)
    (cherry picked from commit 9925b83941eca7535fc4a2c1277d14a9340de90c)

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,59 @@
 import { clsx } from "clsx";
+import CryptoJS from "crypto-js";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+let secretKey = "";
+
+const lowStrengthSecretKey = import.meta.env.VITE_LESS_SECURE_LS_KEY;
+const mediumStrengthSecretKey = import.meta.env.VITE_MEDIUM_SECURE_LS_KEY;
+const highStrengthSecretKey = import.meta.env.VITE_HIGH_SECURE_LS_KEY;
+
+const chooseSecretKey = (strength = "low") => {
+  if (strength === "low") {
+    return lowStrengthSecretKey;
+  } else if (strength === "medium") {
+    return mediumStrengthSecretKey;
+  } else if (strength === "high") {
+    return highStrengthSecretKey;
+  } else {
+    return null;
+  }
+};
+
+// Function to encrypt and store data in localStorage
+export const setLocalSecureItem = (key, value, strength) => {
+  secretKey = chooseSecretKey(strength);
+  const stringValue = JSON.stringify(value);
+
+  try {
+    const encryptedValue = CryptoJS.AES.encrypt(
+      stringValue,
+      secretKey
+    ).toString();
+    localStorage.setItem(key, encryptedValue);
+  } catch (e) {
+    console.log(`Error in encrypting key ${key} and storing data: ${e}`);
+  }
+};
+
+// Function to retrieve and decrypt data from localStorage
+export const getLocalSecureItem = (key, strength) => {
+  secretKey = chooseSecretKey(strength);
+
+  try {
+    const encryptedValue = localStorage.getItem(key);
+    if (encryptedValue) {
+      const decryptedBytes = CryptoJS.AES.decrypt(encryptedValue, secretKey);
+      const decryptedValue = decryptedBytes.toString(CryptoJS.enc.Utf8);
+      return JSON.parse(decryptedValue);
+    }
+    return null;
+  } catch (e) {
+    console.error(`Error retrieving key ${key} and decrypting data: ${e}`);
+    return null;
+  }
+};
